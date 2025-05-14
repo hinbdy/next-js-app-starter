37fb9d297b2975bdbba7837f383a345f0f8f96c8
hinbdy <156166685+hinbdy@users.noreply.github.com>
2025-05-14 10:18:04 +0700

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1747192684063

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_90f597e9._.js",
+  "static/chunks/src_bf72a0c9._.js"
+],
+    source: "dynamic"
+});
